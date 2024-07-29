b9e9f6ef4f33e9f736c32f688145e71b6eba2867
Marton Vago <marci.vago@gmail.com>
2024-07-29 09:24:57 +0200

    refactor: fetch data for projects-id-metadata-view from sample project

--- sprout/app/views/projects_id_metadata_view.py
+++ sprout/app/views/projects_id_metadata_view.py
@@ -3,7 +3,7 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
-from sprout.app.models import Tables
+from sprout.core.repository import load_project
 
 
 def projects_id_metadata_view(request: HttpRequest) -> HttpResponse:
@@ -18,12 +18,11 @@ def projects_id_metadata_view(request: HttpRequest) -> HttpResponse:
         HTTP response that either renders the projects-id-metadata page or redirects
         to create new metadata, update existing metadata, or upload new data.
     """
-    # Columns for each table are prefetched and can be accessed in the Django template
-    # using 'table.columns_set.all'
-    existing_metadata = Tables.objects.prefetch_related("columns_set").all()
+    # TODO: get project identifier from request
+    project = load_project("sample-project")
 
     return render(
         request=request,
         template_name="projects-id-metadata-view.html",
-        context={"existing_metadata": existing_metadata},
+        context={"project": project},
     )
