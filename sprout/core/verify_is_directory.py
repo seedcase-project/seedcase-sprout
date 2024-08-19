27a8dbe9cd520e077e4ab5c4289b4c79df442081
Signe Kirk Brødbæk <signekb@clin.au.dk>
2024-08-19 16:55:22 +0200

    feat: :sparkles: add verify_is_directory()

---
+++ sprout/core/verify_is_directory.py
@@ -0,0 +1,21 @@
+from pathlib import Path
+
+
+def verify_is_directory(path: str):
+    """Verifies if the path is a directory.
+
+    Args:
+        path: The path to verify
+
+    Raises:
+        NotADirectoryError: If the path is not an existing directory
+
+    Returns:
+        path if it's a directory
+    """
+    path = Path(path)
+
+    if not path.is_dir():
+        raise NotADirectoryError(f"{path} is not an existing directory")
+
+    return path
