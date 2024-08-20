00bd023ca6a11db82adfb6e1c62aae42ece35f4c
Marton Vago <marci.vago@gmail.com>
2024-08-20 13:30:07 +0200

    feat: :sparkles: add generate_next_id function

---
+++ sprout/core/generate_next_id.py
@@ -0,0 +1,10 @@
+def generate_next_id(existing_ids: list[int]) -> int:
+    """Generates the next ID in a sequence, given a list of existing IDs. Starts at 1.
+
+    Args:
+        existing_ids: A list of existing IDs
+
+    Returns:
+        The newly generated ID
+    """
+    return max(existing_ids) + 1 if existing_ids else 1
