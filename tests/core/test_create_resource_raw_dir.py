c9ca4920950cb7bf5e536e9265298753da85481e
Signe Kirk Brødbæk <signekb@clin.au.dk>
2024-08-20 14:01:53 +0200

    docs: fix docstring

--- tests/core/test_create_resource_raw_dir.py
+++ tests/core/test_create_resource_raw_dir.py
@@ -2,7 +2,7 @@ from sprout.core.create_resource_raw_dir import create_resource_raw_dir
 
 
 def test_create_resource_raw_dir_returns_new_resource_raw_directory(tmp_path):
-    """Test that the create_resource_raw_dir function returns the directory if it exists.
+    """Test that the create_resource_raw_dir function returns the created raw directory.
     Args:
         tmp_path: Temporary directory created by pytest
     """
