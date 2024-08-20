6ab79785e3777c12c33914e9acb25729996c9eba
Luke W. Johnston <lwjohnst@gmail.com>
2024-08-20 16:42:44 +0200

    refactor: :recycle: output just a path, not creating it yet

--- sprout/core/create_resource_raw_dir.py
+++ sprout/core/create_resource_raw_dir.py
@@ -1,16 +1,14 @@
 from pathlib import Path
 
 
-def create_resource_raw_dir(path: Path) -> Path:
-    """Create raw directory for a resource.
+def create_resource_raw_path(path: Path) -> Path:
+    """Create a path to the raw directory for a resource.
 
     Args:
         path: Directory to create the new resource directory in.
 
     Returns:
-        Directory for the new resource.
+        A path to the folder.
     """
-    resource_raw_dir = path / "raw"
-    resource_raw_dir.mkdir()
 
-    return resource_raw_dir
+    return path / "raw"
--- tests/core/test_create_resource_raw_dir.py
+++ tests/core/test_create_resource_raw_dir.py
@@ -1,9 +1,6 @@
-from sprout.core.create_resource_raw_dir import create_resource_raw_dir
+from sprout.core.create_resource_raw_path import create_resource_raw_path
 
 
-def test_create_resource_raw_dir_returns_new_resource_raw_directory(tmp_path):
-    """Test that the create_resource_raw_dir function returns the created raw directory.
-    Args:
-        tmp_path: Temporary directory created by pytest
-    """
-    assert create_resource_raw_dir(tmp_path) == tmp_path / "raw"
+def test_outputs_correct_raw_directory_path(tmp_path):
+    """The raw directory path is output correctly."""
+    assert create_resource_raw_path(tmp_path) == tmp_path / "raw"
