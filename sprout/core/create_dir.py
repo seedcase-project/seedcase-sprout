a13795d07b0e35313539930c1a5d12aa0a1fa76f
Luke W. Johnston <lwjohnst@gmail.com>
2024-08-20 17:28:54 +0200

    refactor: :recycle: create multiple directories at once

--- sprout/core/create_dir.py
+++ sprout/core/create_dir.py
@@ -1,8 +1,21 @@
 from pathlib import Path
 
 
+def create_dirs(path: list[Path]) -> list[Path]:
+    """Creates directories from a list of directories.
+
+    Args:
+        path: The path pointing to the new directory to create
+    Raises:
+        NoDirectoryError: If the directory wasn't created
+    Returns:
+        path to the newly created directory.
+    """
+    return [create_dir(p) for p in path]
+
+
 def create_dir(path: Path) -> Path:
-    """Creates a directory in the main folder.
+    """Creates a directory from a path.
 
     Args:
         path: The path pointing to the new directory to create
--- tests/core/test_create_dir.py
+++ tests/core/test_create_dir.py
@@ -1,31 +1,61 @@
-from sprout.core.create_dir import create_dir
+from sprout.core.create_dirs import create_dirs
 
 
-def test_create_dir_existing_folder(tmp_path):
-    """Checks for an existing folder and returns the name
-    if the folder already exists.
+def test_creating_existing_directory(tmp_path):
+    """Checks for an existing directory and returns the name
+    if the directory already exists.
     """
+    # Create a list of temporary directories
+    temp_dirs = [
+        tmp_path / "existing_folder",
+        tmp_path / "existing_folder2",
+    ]
+
+    for dir in temp_dirs:
+        dir.mkdir()
+
+    result = create_dirs(temp_dirs)
+
+    # Check if the result is equal to the existing paths
+    assert result == temp_dirs
+
+    # Check the number of directories created
+    assert len(result) == len(temp_dirs)
+
+
+def test_create_single_dir(tmp_path):
+    """Create a new directory and returns the path correctly."""
     # Create a temporary directory
-    folder_path = tmp_path / "existing_folder"
-    folder_path.mkdir()
+    new_dir = [tmp_path / "new_dir"]
 
-    # Call the create_dir function
-    result = create_dir(folder_path)
+    result = create_dirs(new_dir)
 
-    # Check if the result is equal to the folder path
-    assert result == folder_path
+    # Check if the dir is created
+    assert new_dir[0].exists()
+
+    # Check if the result is equal to the new directory
+    assert result[0] == new_dir[0]
+
+    # Check that only one directory is created
+    assert len(result) == len(new_dir)
 
 
-def test_create_dir_new_folder(tmp_path):
-    """Create a new folder and returns the path correctly."""
+def test_create_two_dirs(tmp_path):
+    """Create a new directory and returns the path correctly."""
     # Create a temporary directory
-    folder_path = tmp_path / "new_folder"
+    new_dirs = [
+        tmp_path / "new_dir1",
+        tmp_path / "new_dir2",
+    ]
 
-    # Call the create_dir function
-    result = create_dir(folder_path)
+    result = create_dirs(new_dirs)
 
-    # Check if the folder is created
-    assert folder_path.exists()
+    # Check if the dirs are created
+    assert new_dirs[0].exists()
+    assert new_dirs[1].exists()
 
-    # Check if the result is equal to the folder path
-    assert result == folder_path
+    # Check if the result is equal to the new directory
+    assert result == new_dirs
+
+    # Check that two directories were created
+    assert len(result) == len(new_dirs)
