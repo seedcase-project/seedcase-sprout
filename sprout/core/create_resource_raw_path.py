2a8fc61123fa8d129302b4f342aa1140d16090ae
Signe Kirk Brødbæk <40836345+signekb@users.noreply.github.com>
2024-08-21 11:37:58 +0200

    docs: apply suggestions from code review
    Co-authored-by: martonvago <57952344+martonvago@users.noreply.github.com>

--- sprout/core/create_resource_raw_path.py
+++ sprout/core/create_resource_raw_path.py
@@ -5,7 +5,7 @@ def create_resource_raw_path(path: Path) -> Path:
     """Create a path to the raw directory for a resource.
 
     Args:
-        path: Directory to create the new resource directory in.
+        path: Directory to create the new raw directory in.
 
     Returns:
         A path to the folder.
