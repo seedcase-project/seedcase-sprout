28cfb166b62d9cb38de139a68f644ca75b8fa5f7
martonvago <57952344+martonvago@users.noreply.github.com>
2024-08-21 11:11:34 +0200

    apply suggestions from code review
    Co-authored-by: Luke W. Johnston <lwjohnst86@users.noreply.github.com>

--- sprout/core/create_next_id.py
+++ sprout/core/create_next_id.py
@@ -1,10 +1,10 @@
-def create_next_id(existing_ids: list[int]) -> int:
-    """Generates the next ID in a sequence, given a list of existing IDs. Starts at 1.
+def create_next_id(ids: list[int]) -> int:
+    """Creates the next ID in a sequence, given a list of existing IDs. Starts at 1.
 
     Args:
-        existing_ids: A list of existing IDs
+        ids: A list of existing IDs.
 
     Returns:
-        The newly generated ID
+        The newly generated ID.
     """
-    return max(existing_ids) + 1 if existing_ids else 1
+    return max(ids) + 1 if ids else 1
