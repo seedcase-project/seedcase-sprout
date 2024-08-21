3d9ea48836380ac661ede68a9f05ca3cff85d4a1
Signe Kirk Brødbæk <signekb@clin.au.dk>
2024-08-21 14:41:01 +0200

    docs: align/shorten docstrings

--- tests/core/test_get_ids.py
+++ tests/core/test_get_ids.py
@@ -2,7 +2,7 @@ from sprout.core.get_ids import get_ids
 
 
 def test_get_single_id(tmp_path):
-    """Test that the get_ids function returns a single ID."""
+    """Return a list of a single ID/integer."""
     (tmp_path / "1").mkdir()
     assert get_ids(tmp_path) == [1]
 
@@ -23,7 +23,7 @@ def test_empty_list_when_no_ids(tmp_path):
 
 
 def test_return_multiple_ids(tmp_path):
-    """Test that the get_ids function returns multiple IDs."""
+    """Return multiple IDs."""
     (tmp_path / "1").mkdir()
     (tmp_path / "2").mkdir()
 
@@ -31,7 +31,7 @@ def test_return_multiple_ids(tmp_path):
 
 
 def test_only_numbers_output(tmp_path):
-    """Test that the get_ids function returns only numbers."""
+    """Return only directories with IDs/numbers only."""
     (tmp_path / "1").mkdir()
     (tmp_path / "a").mkdir()
 
@@ -39,7 +39,7 @@ def test_only_numbers_output(tmp_path):
 
 
 def test_different_numbers_output(tmp_path):
-    """Test that the get_ids function returns numbers of any size."""
+    """Return numbers of any size."""
     (tmp_path / "1").mkdir()
     (tmp_path / "20").mkdir()
     (tmp_path / "999").mkdir()
