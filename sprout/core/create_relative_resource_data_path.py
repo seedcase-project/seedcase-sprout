99077a37ae57771f51f3792c07402e19996ef5f5
Signe Kirk Brødbæk <signekb@clin.au.dk>
2024-08-23 14:09:57 +0200

    refactor: :art: avoid hardcoding folder names when extracting relative path
    Co-authored-by: martonvago <57952344+martonvago@users.noreply.github.com>

--- sprout/core/create_relative_resource_data_path.py
+++ sprout/core/create_relative_resource_data_path.py
@@ -1,5 +1,4 @@
 from pathlib import Path
-from re import split
 
 
 def create_relative_resource_data_path(path: Path) -> Path:
@@ -12,6 +11,4 @@ def create_relative_resource_data_path(path: Path) -> Path:
         Relative path from the package root to the resource data file
         E.g., "resources/1/data.parquet"
     """
-    relative_resource_path = split(r"packages/[0-9]*/", str(path))[1]
-
-    return Path(relative_resource_path) / "data.parquet"
+    return Path(*path.parts[-2:]) / "data.parquet"
