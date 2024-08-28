4608eb81f1f09f0d8c0d10f3661e7179eba51d7e
Signe Kirk Brødbæk <signekb@clin.au.dk>
2024-08-28 12:12:53 +0200

    test: :white_check_mark: add test for when a resource exists already

--- tests/core/test_create_resource_structure.py
+++ tests/core/test_create_resource_structure.py
@@ -3,13 +3,26 @@ from pytest import raises
 import sprout.core as sp
 
 
-def test_returns_resource_structure(tmp_path):
+def test_returns_resource_structure_when_no_resources_exists(tmp_path):
     """Returns the paths to the resource and raw data folder."""
     lst_created_paths = sp.create_resource_structure(tmp_path)
 
     assert lst_created_paths[0].is_dir()
     assert lst_created_paths[1].is_dir()
     assert lst_created_paths == [tmp_path / "1", tmp_path / "1" / "raw"]
+
+
+def test_returns_resource_structure_when_resources_exists(tmp_path):
+    """Returns the paths to the resource and raw data folder with the next id."""
+    # Create first resource structure
+    sp.create_resource_structure(tmp_path)
+
+    # Create new resource structure
+    lst_created_paths = sp.create_resource_structure(tmp_path)
+
+    assert lst_created_paths[0].is_dir()
+    assert lst_created_paths[1].is_dir()
+    assert lst_created_paths == [tmp_path / "2", tmp_path / "2" / "raw"]
 
 
 def test_raises_not_a_directory_error(tmp_path):
