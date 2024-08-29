c9d89e89f0caaed0a87c598d525f66fa7b27aeeb
Signe Kirk Brødbæk <signekb@clin.au.dk>
2024-08-29 16:30:12 +0200

    test: :white_check_mark: add tests for write_file()

---
+++ tests/core/test_write_file.py
@@ -0,0 +1,24 @@
+from pytest import fixture, mark
+
+from sprout.core.write_file import write_file
+
+
+@fixture
+def file_content():
+    return "This is test content."
+
+
+def test_returns_path(tmp_path, file_content):
+    """Tests that the path is returned."""
+    file_path = tmp_path / "test.txt"
+
+    assert write_file(file_path, file_content) == file_path
+
+
+def test_creates_a_file_with_the_given_content(tmp_path, file_content):
+    """Tests that a file with the given content is created."""
+    file_path = tmp_path / "test.md"
+
+    assert write_file(file_path, file_content) == file_path
+    assert file_path.is_file() is True
+    assert file_path.read_text() == file_content
