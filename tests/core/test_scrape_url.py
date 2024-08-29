45593f06ebdacae5a1fbe71a8560fc9f357ded73
Signe Kirk Brødbæk <signekb@clin.au.dk>
2024-08-29 13:52:06 +0200

    test: :white_check_mark: add tests for scrape_url()

---
+++ tests/core/test_scrape_url.py
@@ -0,0 +1,37 @@
+from pytest import raises
+from requests import JSONDecodeError
+
+from sprout.core.scrape_url import scrape_url
+
+
+def test_returns_dict_with_expected_content_when_url_contains_json():
+    url = "https://datapackage.org/profiles/2.0/datapackage.json"
+    url_content = scrape_url(url)
+
+    expected_keys = [
+        "$schema",
+        "title",
+        "description",
+        "type",
+        "required",
+        "properties",
+    ]
+    expected_content_except = (
+        "'$schema': 'http://json-schema.org/draft-07/schema#', "
+        "'title': 'Data Package', "
+        "'description': 'Data Package', "
+        "'type': 'object', "
+        "'required': ['resources'], "
+    )
+
+    assert isinstance(url_content, dict)
+    assert expected_keys == list(url_content.keys())
+    assert expected_content_except in str(url_content)
+
+
+def test_raises_error_when_url_does_not_contain_json():
+    url = "https://sprout.seedcase-project.org/"
+    with raises(
+        JSONDecodeError,
+    ):
+        scrape_url(url)
