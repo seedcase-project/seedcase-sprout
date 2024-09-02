5b1d726ece8971497ce82dbd5a45d34590625819
Signe Kirk Brødbæk <signekb@clin.au.dk>
2024-09-02 13:47:22 +0200

    docs: :memo: add that parent folder must exist, otherwise FileNotFoundError is raised

--- sprout/core/write_file.py
+++ sprout/core/write_file.py
@@ -4,12 +4,17 @@ from pathlib import Path
 def write_file(string: str, path: Path) -> Path:
     """Writes a file to the given path with the given content.
 
+    The parent folder of the file given in path must exist.
+
     Args:
         string: Content to be written to the file.
         path: Path of the file to be created, including file name and extension.
 
     Returns:
         A path to the file that was created.
+
+    Raises:
+        FileNotFoundError: If the parent folder of the file doesn't exist.
     """
     path.write_text(string)
 
