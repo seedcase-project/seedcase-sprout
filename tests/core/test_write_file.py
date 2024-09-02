a68b9f290cb15563c93286821570e4bfe953a23c
Signe Kirk Brødbæk <40836345+signekb@users.noreply.github.com>
2024-09-02 10:01:51 +0200

    Apply suggestions from code review
    Co-authored-by: martonvago <57952344+martonvago@users.noreply.github.com>

--- tests/core/test_write_file.py
+++ tests/core/test_write_file.py
@@ -20,5 +20,5 @@ def test_creates_a_file_with_the_given_content(tmp_path, file_content):
     file_path = tmp_path / "test.md"
 
     assert write_file(file_path, file_content) == file_path
-    assert file_path.is_file() is True
+    assert file_path.is_file()
     assert file_path.read_text() == file_content
