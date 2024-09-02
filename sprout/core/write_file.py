8371c5e20890f6624a324ebde3503dd4b77e26ee
Signe Kirk Brødbæk <40836345+signekb@users.noreply.github.com>
2024-09-02 13:30:06 +0200

    Apply suggestions from code review
    Co-authored-by: Luke W. Johnston <lwjohnst86@users.noreply.github.com>

--- sprout/core/write_file.py
+++ sprout/core/write_file.py
@@ -1,12 +1,12 @@
 from pathlib import Path
 
 
-def write_file(path: Path, string: str) -> Path:
+def write_file(string: str, path: Path) -> Path:
     """Writes a file to the given path with the given content.
 
     Args:
-        path: Path of the file to be created, including file name and extension.
         string: Content to be written to the file.
+        path: Path of the file to be created, including file name and extension.
 
     Returns:
         A path to the file that was created.
--- tests/core/test_write_file.py
+++ tests/core/test_write_file.py
@@ -12,13 +12,13 @@ def test_returns_path(tmp_path, file_content):
     """Tests that the path is returned."""
     file_path = tmp_path / "test.txt"
 
-    assert write_file(file_path, file_content) == file_path
+    assert write_file(file_content, file_path) == file_path
 
 
 def test_creates_a_file_with_the_given_content(tmp_path, file_content):
     """Tests that a file with the given content is created."""
     file_path = tmp_path / "test.md"
 
-    assert write_file(file_path, file_content) == file_path
+    assert write_file(file_content, file_path) == file_path
     assert file_path.is_file()
     assert file_path.read_text() == file_content
