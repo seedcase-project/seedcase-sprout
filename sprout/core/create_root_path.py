a16e1137a333138bb2d6f6e257be7613237cc545
Signe Kirk Brødbæk <signekb@clin.au.dk>
2024-09-11 14:08:44 +0200

    feat: :sparkles: add create_root_path()

---
+++ sprout/core/create_root_path.py
@@ -0,0 +1,14 @@
+from pathlib import Path
+
+from platformdirs import user_data_path
+
+
+def create_root_path() -> Path:
+    """Creates the path to Sprout root.
+
+    Returns:
+        A Path with the Sprout root directory.
+    """
+    root_path = user_data_path("sprout")
+
+    return root_path
