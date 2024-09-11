a46df404f9f3ba87072f23c5064b627ea5ef43a1
Signe Kirk Brødbæk <signekb@clin.au.dk>
2024-09-11 14:20:26 +0200

    docs: :memo: specify that the returned Path is tied to the suer

--- sprout/core/create_root_path.py
+++ sprout/core/create_root_path.py
@@ -7,7 +7,7 @@ def create_root_path() -> Path:
     """Creates the path to Sprout root.
 
     Returns:
-        A Path with the Sprout root directory.
+        A Path with the Sprout root directory tied to the user.
     """
     root_path = user_data_path("sprout")
 
