fd734b09c90be4b8eababab36639a3c5e2a29e80
Signe Kirk Brødbæk <signekb@clin.au.dk>
2024-09-11 09:42:07 +0200

    test: :white_check_mark: add tests for get_root_envvar()

---
+++ tests/core/test_get_root_envvar.py
@@ -0,0 +1,24 @@
+import os
+
+from sprout.core.get_root_envvar import get_root_envvar
+
+
+def test_returns_root_envvar_if_set(tmp_path):
+    """Returns the root environment variable SPROUT_ROOT if it is set."""
+    root = tmp_path / "sprout"
+    root.mkdir
+    os.environ["SPROUT_ROOT"] = str(root)
+
+    # Act
+    root_path = get_root_envvar()
+
+    # Assert
+    assert root_path == root
+
+
+def test_returns_none_if_root_envvar_is_not_set():
+    """Returns None if SPROUT_ROOT isn't set."""
+    root_path = get_root_envvar()
+
+    # Assert
+    assert root_path is None
