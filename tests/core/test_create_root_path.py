3537de78c2d8ab8b98b031188f2b64109a1327d7
Signe Kirk Brødbæk <40836345+signekb@users.noreply.github.com>
2024-09-12 09:50:02 +0200

    docs: fix typo
    Co-authored-by: Luke W. Johnston <lwjohnst86@users.noreply.github.com>

--- tests/core/test_create_root_path.py
+++ tests/core/test_create_root_path.py
@@ -2,7 +2,7 @@ from sprout.core.create_root_path import create_root_path
 
 
 def test_returns_root_path():
-    """Returns root path o."""
+    """Returns root path."""
     root_path = create_root_path()
 
     assert root_path.name == "sprout"
