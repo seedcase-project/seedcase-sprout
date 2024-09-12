b767b13c71e8cc96e2a506cec7ee57015a1006f5
Signe Kirk Brødbæk <signekb@clin.au.dk>
2024-09-12 16:51:20 +0200

    test: :white_check_mark: simplify test

--- tests/core/test_create_root_path.py
+++ tests/core/test_create_root_path.py
@@ -3,6 +3,4 @@ from sprout.core.create_root_path import create_root_path
 
 def test_returns_root_path():
     """Returns root path."""
-    root_path = create_root_path()
-
-    assert root_path.name == "sprout"
+    assert create_root_path().name == "sprout"
