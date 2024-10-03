4c0b143ce416ab8874c0e979fd05880bc79b7a5d
Luke W. Johnston <lwjohnst86@users.noreply.github.com>
2024-10-03 10:57:07 +0200

    docs: :pencil2: add comments from review
    Co-authored-by: martonvago <57952344+martonvago@users.noreply.github.com>

--- sprout/core/create_readme_text.py
+++ sprout/core/create_readme_text.py
@@ -10,15 +10,14 @@ def create_readme_text(properties: dict) -> str:
     Returns:
       A string for the README text
     """
-    properties = json.loads(properties)
     # TODO: Finish this once Properties have been finished.
     # resources = # Python code to convert the resource details in the dict as
     # a Markdown list.
 
     readme_text = (
         f"This is the README file for the package.\n\n"
-        f"{properties.name}: {properties.title}\n\n"
+        f"{properties["name"]}: {properties["title"]}\n\n"
         f"Within this data package contains:\n\n"
-        f"{properties.resources}"
+        f"{properties["resources"]}"
     )
     return readme_text
