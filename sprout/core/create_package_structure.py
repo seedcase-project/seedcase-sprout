ee6d443524e62a07e064d385cfb8ddc325f3ef79
Luke W. Johnston <lwjohnst@gmail.com>
2024-10-15 15:35:43 -0400

    docs: :memo: update docstrings with new change

--- sprout/core/create_package_structure.py
+++ sprout/core/create_package_structure.py
@@ -27,8 +27,8 @@ def create_package_structure(path: Path) -> list[Path]:
             to provide the correct path location.
 
     Returns:
-        A list of paths to the two created files: The datapackage.json and the
-            README.md.
+        A list of paths to the two created files, the datapackage.json and the
+            README.md, as well as one created `resources/` folder.
 
     Raises:
         NotADirectoryError: If the directory in the path doesn't exist.
