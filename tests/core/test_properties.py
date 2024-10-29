df1fdd12da40523cb8ff28bd1fbd6869afed241e
Signe Kirk Brødbæk <signekb@clin.au.dk>
2024-10-29 10:39:12 +0100

    test: :white_check_mark: update properties tests to updated Properties classes

--- tests/core/test_properties.py
+++ tests/core/test_properties.py
@@ -35,8 +35,86 @@ from sprout.core.properties import (
         PackageProperties,
     ],
 )
-def test_asdict_generates_dictionary(cls):
-    """Should return a dictionary representation of the object."""
-    properties = cls()
+def test_initiated_class_only_contains_none_values(cls):
+    """When a Properties class is instantiated, all values should be None."""
+    assert all(value is None for value in asdict(cls()).values())
 
-    assert properties.asdict == asdict(properties)
+
+@mark.parametrize(
+    "cls",
+    [
+        ContributorProperties,
+        LicenseProperties,
+        SourceProperties,
+        TableDialectProperties,
+        ReferenceProperties,
+        TableSchemaForeignKeyProperties,
+        MissingValueProperties,
+        ConstraintsProperties,
+        FieldProperties,
+        TableSchemaProperties,
+        ResourceProperties,
+        PackageProperties,
+    ],
+)
+def test_as_pruned_dict_generates_empty_dictionary_when_no_args_given(cls):
+    """Should return an empty dictionary, when no arguments are given to Properties
+    class"""
+    properties_dict = cls().as_pruned_dict
+
+    assert isinstance(properties_dict, dict)
+    assert len(properties_dict) == 0
+
+
+@mark.parametrize(
+    "properties, expected_dict",
+    [
+        (ContributorProperties(family_name="Kirk"), {"family_name": "Kirk"}),
+        (LicenseProperties(name="MIT"), {"name": "MIT"}),
+        (SourceProperties(title="SDCA"), {"title": "SDCA"}),
+        (TableDialectProperties(header=True), {"header": True}),
+        (ReferenceProperties(resource="resource-1"), {"resource": "resource-1"}),
+        (TableSchemaForeignKeyProperties(fields=["field-1"]), {"fields": ["field-1"]}),
+        (MissingValueProperties(value="NA"), {"value": "NA"}),
+        (ConstraintsProperties(required=False), {"required": False}),
+        (FieldProperties(name="field-1"), {"name": "field-1"}),
+        (TableSchemaProperties(fields=["field-1"]), {"fields": ["field-1"]}),
+        (ResourceProperties(name="resource-1"), {"name": "resource-1"}),
+        (
+            PackageProperties(name="package-1", version="3.2.1"),
+            {"name": "package-1", "version": "3.2.1"},
+        ),
+    ],
+)
+def test_as_pruned_dict_preserves_only_non_none_values(properties, expected_dict):
+    """Should return a dictionary with only non-None values when args given."""
+    assert properties.as_pruned_dict == expected_dict
+
+
+@mark.parametrize(
+    "cls",
+    [
+        ContributorProperties,
+        LicenseProperties,
+        SourceProperties,
+        TableDialectProperties,
+        ReferenceProperties,
+        TableSchemaForeignKeyProperties,
+        MissingValueProperties,
+        ConstraintsProperties,
+        FieldProperties,
+        TableSchemaProperties,
+        ResourceProperties,
+        PackageProperties,
+    ],
+)
+def test_default_return_properties_objects_with_same_keys_as_just_instantiated_object(
+    cls,
+):
+    """Should return a Properties object with the same keys as a just-instantiated
+    object, but different values."""
+    default_properties = cls.default()
+
+    assert asdict(default_properties).keys() == asdict(cls()).keys()
+    assert asdict(default_properties).values() != asdict(cls()).values()
+
