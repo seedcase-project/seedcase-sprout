78b65079485110152315b429cbd010f1d243cc02
Signe Kirk Brødbæk <signekb@clin.au.dk>
2024-10-30 13:02:44 +0100

    test: :white_check_mark: simplify test of Properties property by only using one subclass
    Co-authored by @martonvago

--- tests/core/test_properties.py
+++ tests/core/test_properties.py
@@ -40,29 +40,11 @@ def test_initiated_class_only_contains_none_values(cls):
     assert all(value is None for value in asdict(cls()).values())
 
 
-@mark.parametrize(
-    "cls",
-    [
-        ContributorProperties,
-        LicenseProperties,
-        SourceProperties,
-        TableDialectProperties,
-        ReferenceProperties,
-        TableSchemaForeignKeyProperties,
-        MissingValueProperties,
-        ConstraintsProperties,
-        FieldProperties,
-        TableSchemaProperties,
-        ResourceProperties,
-        PackageProperties,
-    ],
-)
-def test_as_pruned_dict_generates_empty_dictionary_when_no_args_given(cls):
+def test_as_pruned_dict_generates_empty_dictionary_when_no_args_given():
     """Should return an empty dictionary, when no arguments are given to Properties
-    class"""
-    properties_dict = cls().as_pruned_dict
-
-    assert properties_dict == {}
+    class."""
+    # Since this is a test of the base class, it's enough to test only one subclass
+    assert ResourceProperties().as_pruned_dict == {}
 
 
 @mark.parametrize(
