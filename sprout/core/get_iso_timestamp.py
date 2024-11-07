e09966625bd4ad399923170fbd31b75e02746457
Signe Kirk Brødbæk <signekb@clin.au.dk>
2024-11-07 16:17:14 +0100

    feat: :sparkles: add helper function `get_compact_iso_timestap()` with tests
    This function generates a compact version of the iso timestamp from `get_iso_timestamp(), by removing characters as ":", "T", and "-". It does add a "-" between the data and time for readability.

--- sprout/core/get_iso_timestamp.py
+++ sprout/core/get_iso_timestamp.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from re import sub
 
 
 def get_iso_timestamp() -> str:
@@ -8,3 +9,12 @@ def get_iso_timestamp() -> str:
         The timestamp as a string. E.g. `2024-05-14T05:00:01+00:00`.
     """
     return datetime.now().astimezone().isoformat(timespec="seconds")
+
+
+def get_compact_iso_timestamp() -> str:
+    """Generates a compact timestamp.
+
+    Returns:
+        The timestamp as a string. E.g. `20240514-05000100`.
+    """
+    return sub(r"\W", "", get_iso_timestamp()).replace("T", "-")
--- tests/core/test_get_iso_timestamp.py
+++ tests/core/test_get_iso_timestamp.py
@@ -3,7 +3,7 @@ from zoneinfo import ZoneInfo
 
 import time_machine
 
-from sprout.core.get_iso_timestamp import get_iso_timestamp
+from sprout.core.get_iso_timestamp import get_compact_iso_timestamp, get_iso_timestamp
 
 
 @time_machine.travel(datetime(2024, 5, 14, 5, 0, 1), tick=False)
@@ -18,3 +18,9 @@ def test_formats_timestamp_in_utc_correctly():
 def test_formats_timestamp_in_other_timezone_correctly():
     """Should return a correctly formatted timestamp in a specific timezone."""
     assert get_iso_timestamp() == "2024-05-14T05:00:01+02:00"
+
+
+@time_machine.travel(datetime(2024, 5, 14, 5, 0, 1), tick=False)
+def test_removes_nonnumeric_characters_correctly():
+    """Should return a timestamp with only numeric characters."""
+    assert get_compact_iso_timestamp() == "20240514-0500010000"
