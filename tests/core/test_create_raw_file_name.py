7dd485d609f0bdf8838f04441427ce0131649b43
Signe Kirk Brødbæk <signekb@clin.au.dk>
2024-11-07 16:20:40 +0100

    feat: :sparkles: add `create_raw_file_name()` with a test

---
+++ tests/core/test_create_raw_file_name.py
@@ -0,0 +1,16 @@
+from datetime import datetime
+from pathlib import Path
+from unittest.mock import patch
+from uuid import UUID
+
+import time_machine
+
+from sprout.core.create_raw_file_name import create_raw_file_name
+
+
+@patch("sprout.core.create_raw_file_name.uuid4", return_value=UUID(int=1))
+@time_machine.travel(datetime(2024, 5, 14, 5, 0, 1), tick=False)
+def test_returns_raw_file_name(mock_uuid, tmp_path):
+    path = create_raw_file_name(Path(tmp_path) / "test.csv")
+
+    assert path == f"20240514-0500010000-{mock_uuid()}.csv.gz"
