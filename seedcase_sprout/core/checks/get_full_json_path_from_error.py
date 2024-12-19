3586c513395944d7027e6995f679b2f8bcb46497
martonvago <marci.vago@gmail.com>
2024-12-19 18:13:12 +0000

    refactor: :recycle: return early from function

--- seedcase_sprout/core/checks/get_full_json_path_from_error.py
+++ seedcase_sprout/core/checks/get_full_json_path_from_error.py
@@ -12,9 +12,8 @@ def get_full_json_path_from_error(error: ValidationError) -> str:
     Returns:
         The `json_path`.
     """
-    json_path = error.json_path
     if error.validator == "required":
         match = re.search("'(.*)' is a required property", error.message)
         if match:
-            json_path += f".{match.group(1)}"
-    return json_path
+            return f"{error.json_path}.{match.group(1)}"
+    return error.json_path
