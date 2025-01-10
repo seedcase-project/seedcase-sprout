8196dc088604933ae2f906f784baa7dc16117712
martonvago <marci.vago@gmail.com>
2025-01-10 16:44:38 +0000

    feat: :sparkles: put required fields into constants

---
+++ seedcase_sprout/core/sprout_checks/required_fields.py
@@ -0,0 +1,18 @@
+from seedcase_sprout.core import checks
+from seedcase_sprout.core.checks.required_fields import (
+    RequiredFieldType,
+)
+
+# Sprout-specific required fields and their types
+
+PACKAGE_SPROUT_REQUIRED_FIELDS = checks.PACKAGE_RECOMMENDED_REQUIRED_FIELDS | {
+    "title": RequiredFieldType.str,
+    "description": RequiredFieldType.str,
+    "version": RequiredFieldType.str,
+    "created": RequiredFieldType.str,
+}
+
+RESOURCE_SPROUT_REQUIRED_FIELDS = checks.RESOURCE_REQUIRED_FIELDS | {
+    "title": RequiredFieldType.str,
+    "description": RequiredFieldType.str,
+}
