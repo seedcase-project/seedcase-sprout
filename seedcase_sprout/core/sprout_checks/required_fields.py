6611019bb9e27fdd2566606ddc85a4fc15056c30
martonvago <marci.vago@gmail.com>
2025-01-14 12:08:15 +0000

    refactor: :recycle: make structure of PACKAGE_SPROUT_REQUIRED_FIELDS consistent

--- seedcase_sprout/core/sprout_checks/required_fields.py
+++ seedcase_sprout/core/sprout_checks/required_fields.py
@@ -6,12 +6,16 @@ from seedcase_sprout.core.sprout_checks.omit_keys import omit_keys
 
 # Sprout-specific required fields and their types
 
-PACKAGE_SPROUT_REQUIRED_FIELDS = checks.PACKAGE_RECOMMENDED_FIELDS | {
-    "title": RequiredFieldType.str,
-    "description": RequiredFieldType.str,
-    "version": RequiredFieldType.str,
-    "created": RequiredFieldType.str,
-}
+PACKAGE_SPROUT_REQUIRED_FIELDS = (
+    omit_keys(checks.PACKAGE_REQUIRED_FIELDS, ["resources"])
+    | checks.PACKAGE_RECOMMENDED_FIELDS
+    | {
+        "title": RequiredFieldType.str,
+        "description": RequiredFieldType.str,
+        "version": RequiredFieldType.str,
+        "created": RequiredFieldType.str,
+    }
+)
 
 RESOURCE_SPROUT_REQUIRED_FIELDS = omit_keys(
     checks.RESOURCE_REQUIRED_FIELDS, ["data"]
