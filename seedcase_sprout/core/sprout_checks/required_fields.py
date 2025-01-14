fea4a5fd18098d087c12e5c89345dab975c54dd2
martonvago <marci.vago@gmail.com>
2025-01-14 14:55:17 +0000

    refactor: :recycle: drop fields using pop

--- seedcase_sprout/core/sprout_checks/required_fields.py
+++ seedcase_sprout/core/sprout_checks/required_fields.py
@@ -2,12 +2,11 @@ from seedcase_sprout.core import checks
 from seedcase_sprout.core.checks.required_fields import (
     RequiredFieldType,
 )
-from seedcase_sprout.core.sprout_checks.omit_keys import omit_keys
 
 # Sprout-specific required fields and their types
 
 PACKAGE_SPROUT_REQUIRED_FIELDS = (
-    omit_keys(checks.PACKAGE_REQUIRED_FIELDS, ["resources"])
+    checks.PACKAGE_REQUIRED_FIELDS
     | checks.PACKAGE_RECOMMENDED_FIELDS
     | {
         "title": RequiredFieldType.str,
@@ -16,10 +15,10 @@ PACKAGE_SPROUT_REQUIRED_FIELDS = (
         "created": RequiredFieldType.str,
     }
 )
+PACKAGE_SPROUT_REQUIRED_FIELDS.pop("resources", None)
 
-RESOURCE_SPROUT_REQUIRED_FIELDS = omit_keys(
-    checks.RESOURCE_REQUIRED_FIELDS, ["data"]
-) | {
+RESOURCE_SPROUT_REQUIRED_FIELDS = checks.RESOURCE_REQUIRED_FIELDS | {
     "title": RequiredFieldType.str,
     "description": RequiredFieldType.str,
 }
+RESOURCE_SPROUT_REQUIRED_FIELDS.pop("data", None)
