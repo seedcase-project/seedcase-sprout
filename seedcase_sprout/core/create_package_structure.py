7b13244960d3df69c2a8437c897b8c600fdda337
Luke W. Johnston <lwjohnst@gmail.com>
2025-02-12 15:26:02 +0100

    refactor: :recycle: create only the package folder and `datapackage.json`
    Closes #958

--- seedcase_sprout/core/create_package_structure.py
+++ seedcase_sprout/core/create_package_structure.py
@@ -1,37 +1,38 @@
 from pathlib import Path
 
-from seedcase_sprout.core.check_is_dir import check_is_dir
-from seedcase_sprout.core.create_dirs import create_dir
-from seedcase_sprout.core.create_id_path import create_id_path
-from seedcase_sprout.core.create_next_id import create_next_id
 from seedcase_sprout.core.create_properties_path import create_properties_path
-from seedcase_sprout.core.create_readme_path import create_readme_path
-from seedcase_sprout.core.create_readme_text import create_readme_text
-from seedcase_sprout.core.get_ids import get_ids
 from seedcase_sprout.core.properties import PackageProperties
-from seedcase_sprout.core.write_file import write_file
+from seedcase_sprout.core.sprout_checks.check_package_properties import (
+    check_package_properties,
+)
 from seedcase_sprout.core.write_json import write_json
 
 
-def create_package_structure(path: Path) -> list[Path]:
-    """Sets up the folder structure for a new package.
+def create_package_properties(
+    properties: PackageProperties, path: Path = Path.cwd()
+) -> list[Path]:
+    """Creates a folder and `datapackage.json` file with properties for a new package.
 
-    This is the first function to use to create a new data package. It assigns a package
-    ID and then creates a package folder and all the necessary files for a package
-    (excluding the resources), outputting the file paths for the created files.
+    This is the first function to use to create a new data package. It creates a
+    `datapackage.json` file based on the values you give it with the `properties`
+    argument. You can use the `PackageProperties` class to help create a valid
+    properties object that has the correct fields.
 
     Args:
-        path: The path to the folder containing the packages. Use `path_packages()`
-            to provide the correct path location.
+        properties: A `PackageProperties` object containing the properties for the
+            package.
+        path: The path to the folder where the data package will be created.
+            A folder within the `path` folder is created using `PackageProperties.name`.
+            Defaults to the working directory.
 
     Returns:
-        A list of paths to the two created files, datapackage.json and
-            README.md, and the created folder `resources/`.
+        A path to the created folder and `datapackage.json` file. The newly created
+            folder is the name provided by the value in the `PackageProperties.name`
+            field.
 
     Raises:
-        NotADirectoryError: If the directory in the path doesn't exist.
-        FileNotFoundError: If a file could not be created.
-        TypeError: If `datapackage.json` could not be created.
+        ExceptionGroup: If there is an error in the properties. A group of
+            `CheckError`s, one error per failed check.
 
     Examples:
         ```{python}
@@ -45,22 +46,16 @@ def create_package_structure(path: Path) -> list[Path]:
             temp_path = Path(temp_dir)
 
             # Create a package structure
-            sp.create_package_structure(path=temp_path)
+            sp.create_package_properties(
+                # Using a built-in properties object
+                properties=sp.example_package_properties,
+                path=temp_path
+            )
         ```
     """
-    check_is_dir(path)
-    ids = get_ids(path)
-    id = create_next_id(ids)
-    package_path = create_id_path(path, id)
-    create_dir(package_path)
-
-    properties = PackageProperties.default()
-    properties_path = create_properties_path(package_path)
-    readme = create_readme_text(properties)
-    readme_path = create_readme_path(package_path)
-
-    return [
-        write_json(properties.compact_dict, properties_path),
-        write_file(readme, readme_path),
-        create_dir(package_path / "resources"),
-    ]
+    check_package_properties(
+        properties.compact_dict,
+    )
+    properties_path = create_properties_path(path / properties.name)
+    Path(properties_path).mkdir(parents=True)
+    return [write_json(properties.compact_dict, properties_path)]
