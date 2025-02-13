fa28b985e984a0a1bdbc4a208a513cb776d74ec7
Luke W. Johnston <lwjohnst@gmail.com>
2025-02-13 09:22:09 +0100

    docs: :building_construction: updated and finished pseudocode for `build_resource_parquet()`

--- docs/design/implementation/pseudocode/write_resource_parquet.py
+++ docs/design/implementation/pseudocode/write_resource_parquet.py
@@ -1,17 +1,23 @@
 # ruff: noqa
-def write_resource_parquet(
-    raw_files_path: list[Path], parquet_path: Path, properties_path: Path
+def build_resource_parquet(
+    raw_files_path: list[Path], resource_properties: ResourceProperties
 ) -> Path:
     """Merge all raw resource file(s) and write into a Parquet file.
 
     This function takes the file(s) provided by `raw_files_path` and merges them into
-    a `data.parquet` file provided by `parquet_path`. While Sprout generally assumes
+    a `data.parquet` file. The Parquet file will be stored at the path found in `ResourceProperties.path`.
+    While Sprout generally assumes
     that the files stored in the `resources/raw/` folder have already been
     verified and validated, this function does some quick verification checks
     of the data after reading it into Python from the raw file(s) by comparing
-    with the current properties given by the `properties_path`. All data in the
+    with the current properties given by the `resource_properties`. All data in the
     `resources/raw/` folder will be merged into one single data object and then
     written back to the Parquet file. The Parquet file will be overwritten.
+
+    If there are any duplicate observation units in the data, only the most recent
+    observation unit will be kept. This way, if there are any errors or mistakes
+    in older raw files that has been corrected in later files, the mistake can still
+    be kept, but won't impact the data that will actually be used.
 
     Examples:
 
@@ -29,31 +35,23 @@ def write_resource_parquet(
         raw_files_path: A list of paths for all the raw files, mostly commonly stored in the
             `.csv.gz` format. Use `path_resource_raw_files()` to help provide the
             correct paths to the raw files.
-        parquet_path: The path to the Use `path_resource_data()` to help provide
-            the correct path location for the resource's `data.parquet` file.
-        properties_path: The path to the properties for this resource. Use
-            `path_package_properties()` to help give the correct location.
+        resource_properties: The `ResourceProperties` object that contains the properties
+            of the resource you want to create the Parquet file for.
 
     Returns:
         Outputs the path object of the created Parquet file.
     """
     # Not sure if this is the correct way to verify multiple files.
-    [verify_is_file(path) for path in raw_files_path]
+    [check_is_file(path) for path in raw_files_path]
+    check_resource_properties(resource_properties)
 
-    data = read_raw_files(raw_file)
+    data = read_raw_files(raw_files_path)
+    data = drop_duplicate_obs_units(data)
 
-    # Confirms that the data matches the resource properties found in `datapackage.json`.
-    # Not sure if this is the best solution here to load in the properties file.
-    verify_is_file(properties_path)
-    properties = read_json(properties_path)
+    # This function could be several functions or the one full function.
+    check_data(data, resource_properties)
 
-    # This function could be several, e.g. verify_data_column_types(), verify_data_column_names()?
-    verify_data(data, properties)
-
-    # Could include validation here?
-    # validate_data(data, properties)
-
-    return write_parquet(data, parquet_path)
+    return write_parquet(data, resource_properties["path"])
 
 
 def write_parquet(data: DataFrame, path: Path) -> Path:
@@ -68,12 +66,8 @@ def read_raw_files(paths: list[Path]) -> DataFrame:
     return data
 
 
-def verify_data(data: DataFrame, properties: dict) -> Path:
-    # Compare against the properties for:
-    # - Header names
-    # - Data types
-    # - Number of rows?
-    # - Others?
-
-    # Error if fails, data if pass
-    return data
+def drop_duplicate_obs_units(data: DataFrame) -> DataFrame:
+    # Drop duplicates based on the observation unit, keeping only the most
+    # recent one. This allows older raw files to contain potentially wrong
+    # data that was corrected in the most recent file.
+    return data.drop_duplicates()
