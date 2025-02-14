20c5efdbe34fbe6ace4a6e7e1d24003226527c95
martonvago <marci.vago@gmail.com>
2025-02-14 17:06:24 +0000

    feat: :sparkles: add get_polars_data_type

---
+++ seedcase_sprout/core/sprout_checks/data/get_polars_data_type.py
@@ -0,0 +1,45 @@
+import polars as pl
+
+from seedcase_sprout.core.properties import FieldType
+
+
+def get_polars_data_type(field_type: FieldType | None) -> pl.DataType:
+    """Maps Frictionless field types to polars data types.
+
+    If the Frictionless field type has formatting constraints that are not included
+    in any specialised polars data type, the mapping is to string. The formatting
+    constraints are then checked without polars.
+
+    Args:
+        field_type: The Frictionless field type to map.
+
+    Returns:
+        The polars data type the field is mapped to.
+
+    Raises:
+        NotImplementedError: If Sprout doesn't yet support the Frictionless field type.
+    """
+    match field_type:
+        case "geojson":
+            raise NotImplementedError()
+        case (
+            "string"
+            | "boolean"
+            | "datetime"
+            | "date"
+            | "time"
+            | "year"
+            | "yearmonth"
+            | "duration"
+            | "list"
+            | "array"
+            | "object"
+            | "geopoint"
+        ):
+            return pl.String
+        case "number":
+            return pl.Float64
+        case "integer":
+            return pl.Int64
+        case _:
+            return pl.Unknown
