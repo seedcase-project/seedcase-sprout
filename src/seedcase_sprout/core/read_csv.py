ebc4e7702844c63a00e3de9fbd36d960091a707b
martonvago <marci.vago@gmail.com>
2025-03-11 15:35:37 +0000

    fix: :bug: fix wrong quote

--- src/seedcase_sprout/core/read_csv.py
+++ src/seedcase_sprout/core/read_csv.py
@@ -38,6 +38,6 @@ def read_csv(data_path: Path) -> pl.DataFrame:
             raise ValueError(
                 f"At least one row in the data file at {data_path} contains more items "
                 "than the expected number of columns. Please make sure that all rows "
-                “have the same number of items."
+                "have the same number of items."
             ) from error
         raise
