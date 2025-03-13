07a2c8264c33a320dd5f3b97f0f1955c48b76876
martonvago <57952344+martonvago@users.noreply.github.com>
2025-03-13 08:35:42 +0000

    refactor: :recycle: apply suggestions from code review
    Co-authored-by: Luke W. Johnston <lwjohnst86@users.noreply.github.com>

--- src/seedcase_sprout/core/sprout_checks/check_data_header.py
+++ src/seedcase_sprout/core/sprout_checks/check_data_header.py
@@ -5,12 +5,12 @@ from seedcase_sprout.core.properties import ResourceProperties
 
 
 def check_data_header(
-    data_frame: pl.DataFrame, resource_properties: ResourceProperties
+    data: pl.DataFrame, resource_properties: ResourceProperties
 ) -> pl.DataFrame:
     """Checks that column names in the data frame match column names in the properties.
 
     Args:
-        data_frame: The data frame to check.
+        data: The data frame to check.
         resource_properties: The resource properties to check against.
 
     Returns:
@@ -19,7 +19,7 @@ def check_data_header(
     Raises:
         ValueError: If the header doesn't match the expected column names.
     """
-    data_columns = data_frame.schema.names()
+    data_columns = data.schema.names()
     expected_columns = [
         field.name
         for field in get_nested_attr(resource_properties, "schema.fields", default=[])
@@ -29,4 +29,4 @@ def check_data_header(
             "Column names in the data frame do not match column names in the resource "
             f"properties. Expected {expected_columns}, but found {data_columns}."
         )
-    return data_frame
+    return data
