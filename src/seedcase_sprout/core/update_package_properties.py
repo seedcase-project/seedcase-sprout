b225aec4fe1a9f5eda2652830387723211f35f49
Luke W. Johnston <lwjohnst@gmail.com>
2025-03-19 16:20:31 +0100

    style: :art: ran Ruff formatter

--- src/seedcase_sprout/core/update_package_properties.py
+++ src/seedcase_sprout/core/update_package_properties.py
@@ -1,5 +1,3 @@
-from pathlib import Path
-
 from seedcase_sprout.core.checks.check_error_matcher import CheckErrorMatcher
 from seedcase_sprout.core.properties import PackageProperties
 from seedcase_sprout.core.sprout_checks.check_package_properties import (
