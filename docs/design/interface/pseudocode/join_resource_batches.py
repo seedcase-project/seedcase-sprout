08bc8b23ccf252b43be4c5702ffe85f09071905a
Luke W. Johnston <lwjohnst@gmail.com>
2025-03-19 18:41:14 +0100

    docs: :building_construction: update the pseudocode files to match the flows

---
+++ docs/design/interface/pseudocode/join_resource_batches.py
@@ -0,0 +1,48 @@
+# ruff: noqa
+def join_resource_batches(
+    data_list: list[DataFrame], resource_properties: ResourceProperties
+) -> DataFrame:
+    """Joins all batch resource DataFrames into a single (Polars) DataFrame.
+
+    This function takes the list of DataFrames, merges them together, does a
+    check to confirm the data are ok after joining against the `resource_properties`,
+    and then drops any duplicate observational units.
+
+    If there are any duplicate observational units in the data, only the most recent
+    observational unit will be kept. This way, if there are any errors or mistakes
+    in older batch files that have been corrected in later files, the mistake can still
+    be kept, but won't impact the data that will actually be used.
+
+    Examples:
+
+        ``` python
+        import seedcase_sprout.core as sp
+
+        sp.join_resource_batches(
+            batch_files_path=sp.path_resources_batch_files(1),
+            resource_properties=sp.example_resource_properties,
+        )
+        ```
+
+    Args:
+        data_list: A list of Polars DataFrames for all the batch files.
+        resource_properties: The `ResourceProperties` object that contains the properties
+            of the resource you want to check the data against.
+
+    Returns:
+        Outputs a single DataFrame object of all the batch data.
+    """
+    check_resource_properties(resource_properties)
+
+    data = polars.concat(data_list)
+    data = drop_duplicate_obs_units(data)
+    check_data(data, resource_properties)
+
+    return data
+
+
+def drop_duplicate_obs_units(data: DataFrame) -> DataFrame:
+    # Drop duplicates based on the observation unit, keeping only the most
+    # recent one. This allows older batch files to contain potentially wrong
+    # data that was corrected in the most recent file.
+    return data.drop_duplicates()
--- docs/design/interface/pseudocode/write_resource_batches.py
+++ docs/design/interface/pseudocode/write_resource_batches.py
@@ -1,18 +1,22 @@
 # ruff: noqa
-def write_resource_batch_data(data_path, resource_properties) -> Path:
-    """Writes the original data file into the resource's batch data folder.
+def write_resource_batches(
+    data: pl.DataFrame, resource_properties: ResourceProperties
+) -> Path:
+    """Writes the tidied, original data (as a DataFrame) into the resource's batch data folder.
 
-    Copy the file from `data_path` over into the resource location given by
-    `path`. This will compress the file and use a timestamped, unique file
+    Writes the original raw data that's been read in as a Polars DataFrame
+    into the resource location available in the `path` property of the
+    `resource_properties`. This will save a timestamped, unique file
     name to store it as a backup. See the
     [design](https://sprout.seedcase-project.org/docs/design/) docs for an
-    explanation of this file. Data is always checked against the properties
+    explanation of this batch file. Data is always checked against the properties
     before saving into the batch folder. Copies and compresses the file, and
     outputs the path object of the created file.
 
         Args:
-            data_path: Path to a raw data file that you want Sprout to store
-                into the data package.
+            data: A Polars DataFrame object with the data to write to the batch folder.
+                Use `read_resource_batches()` and `join_resource_batches()` to get the
+                data ready to write.
             resource_properties: The properties object for the specific resource.
                 Use `read_properties()` to read the properties for the resource
                 and `get_properties()` to get the correct resource properties.
@@ -25,28 +29,22 @@ def write_resource_batch_data(data_path, resource_properties) -> Path:
             #| eval: false
             ```
     """
-    check_is_file(data_path)
-    check_is_supported_format(data_path)
-    data = read_data(data_path)
     check_data(data, resource_properties)
     # Since `path` is to the `data.parquet` file.
     batch_dir = Path(resource_properties.path.parent / "batch")
 
-    batch_resource_path = Path(batch_dir / create_batch_file_name(data_path))
-    return write_parquet(data_path, batch_resource_path)
+    batch_resource_path = Path(batch_dir / create_batch_file_name())
+    return write_parquet(data, batch_resource_path)
 
 
-def create_batch_file_name(data_path: Path) -> str:
+def create_batch_file_name() -> str:
     """Creates a timestamped, unique file name for the batch data file.
 
     This function creates a timestamped, unique file name for the batch data file
     that is being copied into the resource's batch data folder. This file name
     is used to store the data file as a backup. The file name will be
-    in the format `{timestamp}-{uuid}.{extension}.gz`, where `timestamp` is the
+    in the format `{timestamp}-{uuid}.parquet`, where `timestamp` is the
     current time following ISO8601 format, and `uuid` is a universally unique ID.
-
-        Args:
-            data_path: The path to the batch data file.
 
         Returns:
             A path to a (potentially) new batch resource file.
@@ -54,5 +52,4 @@ def create_batch_file_name(data_path: Path) -> str:
     # Untested code.
     timestamp = datetime.datetime.now().isoformat()
     uuid = str(uuid.uuid4())
-    extension = data_path.suffix
-    return f"{timestamp}-{uuid}.{extension}.gz"
+    return f"{timestamp}-{uuid}.parquet"
