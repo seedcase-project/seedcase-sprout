a93e73e81492e0550d74deb6b2dd156f41a1276f
Luke W. Johnston <lwjohnst@gmail.com>
2025-03-24 15:50:14 +0100

    docs: :building_construction: pseudocode for `check_data()`

---
+++ docs/design/interface/pseudocode/check_data.py
@@ -0,0 +1,64 @@
+# ruff: noqa
+def check_data(
+    data: DataFrame, resource_properties: ResourceProperties
+) -> DataFrame | Error:
+    """Checks that the DataFrame matches the requirements in the resource properties.
+
+    Runs a few checks to compare between the data and the properties on the items:
+
+    | Data | Properties |
+    |:------|:------------|
+    | Column names | `field.names` |
+    | Column types | `field.types` |
+    | Column values' types | `field.types` |
+    | Column values' constraints | `field.constraints` |
+
+    Error messages output generally in the format of:
+
+    > # {data item}:
+    >
+    > There is a mismatch found:
+    >
+    > - In the properties: {mismatch}
+    > - In the data: {mismatch}
+
+    Args:
+        data: A Polars DataFrame.
+        resource_properties: The specific `ResourceProperties` for the `data`.
+
+    Returns:
+        If there are any issues, it gives an Error. Otherwise, will output the `data`.
+
+    Raises:
+        ExceptionGroup: A list of messages that highlight where there are differences
+            between the data and the properties.
+
+    Examples:
+        ```{python}
+        import seedcase_sprout as sp
+
+        check_data(
+            data=sp.example_data(),
+            resource_properties=sp.example_resource_properties()
+        )
+        ```
+    """
+    check_resource_properties(resource_properties)
+
+    # TODO: These individual checks have their own pseudocode files.
+    error_messages = _check_column_names(data, resource_properties)
+
+    error_messages += _check_column_types(data, resource_properties)
+
+    # TODO: If there are errors in the types above, this next line won't matter. Maybe have an error output here first?
+    if error_messages:
+        raise ExceptionGroup(error_messages, ...)
+
+    error_messages += _check_column_values_types(data, resource_properties)
+
+    error_messages += _check_column_values_constraints(data, resource_properties)
+
+    if error_messages:
+        raise ExceptionGroup(error_messages, ...)
+
+    return data
