dc0d6a33f52737d3b99ff8104926ba564a69ac66
Signe Kirk Brødbæk <signekb@clin.au.dk>
2025-03-26 19:30:11 +0100

    feat: :sparkles: add `read_reasource_batches()` without `check_data()`

---
+++ src/seedcase_sprout/core/read_resource_batches.py
@@ -0,0 +1,138 @@
+from datetime import datetime
+from pathlib import Path
+
+import polars as pl
+
+from seedcase_sprout.core.check_is_file import check_is_file
+
+# from seedcase_sprout.core.checks.check_data import check_data
+from seedcase_sprout.core.checks.check_resource_properties import (
+    check_resource_properties,
+)
+from seedcase_sprout.core.properties import ResourceProperties
+
+
+def read_resource_batches(
+    paths: list[Path], resource_properties: ResourceProperties
+) -> list[pl.DataFrame]:
+    """Reads all the batch resource file(s) into a list of (Polars) DataFrames.
+
+    This function takes the file(s) given by `paths`, reads them in as Polars
+    DataFrames as a list and does some checks on each of the DataFrames in the list
+    based on the `resource_properties`. The `resource_properties` object is used
+    to check the data and ensure it is correct. While Sprout generally assumes
+    that the files stored in the `resources/<id>/batch/` folder are already
+    correctly structured and tidy, this function still runs checks to ensure the
+    data are correct by comparing to the properties.
+
+    Args:
+        paths: A list of paths for all the files in the resource's `batch/` folder.
+            Use `path_resource_batch_files()` to help provide the correct paths to the
+            batch files.
+        resource_properties: The `ResourceProperties` object that contains the properties
+            of the resource you want to check the data against.
+
+    Returns:
+        Outputs a list of DataFrame objects from all the batch files.
+
+    Examples:
+        ``` python
+        import seedcase_sprout.core as sp
+
+        sp.read_resource_batches(
+            paths=sp.path_resources_batch_files(1),
+            resource_properties=sp.example_resource_properties(),
+        )
+        ```
+    """
+    list(map(check_is_file, paths))
+    check_resource_properties(resource_properties.compact_dict)
+
+    data_list = list(map(_read_parquet_batch_file, paths))
+
+    # TODO: Uncomment and test when `check_data` is implemented
+    # list(map(check_data, data_list, resource_properties))
+
+    return data_list
+
+
+def _read_parquet_batch_file(path: Path) -> pl.DataFrame:
+    """Reads a Parquet batch file and adds the timestamp as a column.
+
+    This function reads a Parquet batch file into a Polars DataFrame and adds
+    a timestamp column to the DataFrame, extracted from the file name.
+
+    Args:
+        path: Path the Parquet batch file.
+
+    Returns:
+        The Parquet file as a DataFrame with a timestamp column added.
+
+    Examples:
+        ```{python}
+        _read_parquet_batch_file(Path("resources/1/batch/2025-03-26T153402Z-1.parquet"))
+        ```
+    """
+    data = pl.read_parquet(path)
+    timestamp = _extract_timestamp_from_batch_file_path(path)
+    _check_timestamp_format(timestamp)
+    data = _add_timestamp_as_column(data, timestamp)
+    return data
+
+
+def _extract_timestamp_from_batch_file_path(path: Path) -> str:
+    """Extracts the timestamp from the file name.
+
+    Since the batch file name has been created by `create_batch_file_name()`, with a
+    specific format, the timestamp can be extracted by taking the first 18 characters of
+    the file name.
+    """
+    return path.stem[0:18]
+
+
+def _add_timestamp_as_column(data: pl.DataFrame, timestamp: str) -> pl.DataFrame:
+    """Adds the timestamp as a column to the data.
+
+    Args:
+        data: Data to add timestamp column to.
+        timestamp: Timestamp to add as values in the timestamp column.
+
+    Returns:
+        Data with added timestamp column.
+
+    Examples:
+        ```{python}
+        _add_timestamp_as_column(pl.DataFrame(), "2025-03-26T153402Z")
+        ```
+    """
+    return data.insert_column(0, pl.Series("timestamp", [timestamp] * len(data)))
+
+
+def _check_timestamp_format(timestamp: str, format: str = "%Y-%m-%dT%H%M%SZ") -> str:
+    """Check if the timestamp is in the expected format.
+
+    The expected format is "%Y-%m-%dT%H%M%SZ", which is the format used by
+    `create_batch_file_name()`.
+
+    Args:
+        timestamp: Timestamp string to check.
+        format: Expected format of the timestamp.
+
+    Returns:
+        Timestamp string if in the expected format.
+
+    Raises:
+        ValueError: If the timestamp doesn't fit the expected format.
+
+    Examples:
+        ```{python}
+        _check_timestamp_format("2025-03-26T153402Z")
+        ```
+    """
+    try:
+        datetime.strptime(timestamp, format)
+        return timestamp
+    except ValueError as error:
+        raise ValueError(
+            f"Timestamp '{timestamp}' is not in the expected format {format}"
+        ) from error
