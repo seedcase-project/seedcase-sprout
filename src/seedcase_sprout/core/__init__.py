6c3f0db395d5743cc7e0549e864bd9d1029589e1
Signe Kirk Brødbæk <signekb@clin.au.dk>
2025-03-28 13:52:01 +0100

    refactor: :truck: `example_package_properties.py` -> `example_properties.py`

--- src/seedcase_sprout/core/__init__.py
+++ src/seedcase_sprout/core/__init__.py
@@ -12,7 +12,7 @@
 from .as_readme_text import as_readme_text
 from .create_resource_properties import create_resource_properties
 from .create_resource_structure import create_resource_structure
-from .example_package_properties import example_package_properties
+from .example_properties import example_package_properties
 from .extract_resource_properties import extract_resource_properties
 
 # TODO: Consider having all these in one module.
