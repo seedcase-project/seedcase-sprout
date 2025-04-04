c224992ae4df46a2ef843c5d719ed70446e713d6
Luke W. Johnston <lwjohnst@gmail.com>
2025-04-04 17:20:54 +0200

    docs: :building_construction: revises from review

--- docs/design/interface/pseudocode/_check_column_names.py
+++ docs/design/interface/pseudocode/_check_column_names.py
@@ -12,7 +12,8 @@ def _check_column_names(
         The data if the column names match.
 
     Raises:
-        CheckError: If the column names do not match then the error message will
+        TODO: Update this to the relevant check error.
+        SomeCheckError: If the column names do not match then the error message will
             include which names are mismatching between the `data` and `resource_properties`.
     """
     check_resource_properties(resource_properties)
@@ -20,25 +21,24 @@ def _check_column_names(
     names_in_data = data.schema.names()
     names_in_resource = _get_property_field_names(resource_properties)
 
-    difference_in_data = set(names_in_data) - set(names_in_resource)
-    difference_in_properties = set(names_in_resource) - set(names_in_data)
+    extra_columns_in_data = set(names_in_data) - set(names_in_resource)
+    missing_columns_in_data = set(names_in_resource) - set(names_in_data)
 
-    # Should look something like maybe with both mismatch:
-    # "Mismatch between names in data vs properties: "
-    #
-    # "- Data: {difference_in_names}."
-    # "- Resource properties: {names_in_resource}."
-    #
-    # Or if mismatch in e.g. data only:
-    # "Mismatch between names in data vs properties: "
-    #
-    # "- Data: {difference_in_names}."
-    # TODO: This function name is a placeholder for getting the message to look like above.
-    error_message = _format_column_name_error_message(
-        difference_in_data, difference_in_properties
-    )
-
-    if error_message:
-        raise CheckError(error_message)
+    if extra_columns_in_data or missing_columns_in_data:
+        # The error message could look something like the below:
+        # "Columns in data that are missing or extra when compared to properties: "
+        #
+        # "- Extras: {extra_columns_in_data}."
+        # "- Missing: {missing_columns_in_data}."
+        #
+        # Or if mismatch in e.g. data only:
+        # "Columns in data that are not in the properties: "
+        #
+        # "- Columns: {difference_in_names}."
+        # TODO: This function name is a placeholder for getting the message to look like above.
+        error_message = _format_column_name_error_message(
+            extra_columns_in_data, missing_columns_in_data
+        )
+        raise SomeCheckError(error_message)
 
     return data
