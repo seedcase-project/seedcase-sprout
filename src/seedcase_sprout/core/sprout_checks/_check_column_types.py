1532c94150e63d4c7fd7129c7cf4d11468038513
martonvago <marci.vago@gmail.com>
2025-04-07 14:23:07 +0100

    refactor: :recycle: make check_column_types take data frame as input

---
+++ src/seedcase_sprout/core/sprout_checks/_check_column_types.py
@@ -0,0 +1,121 @@
+from typing import Callable, TypedDict
+
+import polars as pl
+
+from seedcase_sprout.core.get_nested_attr import get_nested_attr
+from seedcase_sprout.core.properties import (
+    FieldProperties,
+    FieldType,
+    ResourceProperties,
+)
+
+
+def _check_column_types(
+    data: pl.DataFrame, resource_properties: ResourceProperties
+) -> pl.DataFrame:
+    """Checks that column data types match the data types specified in the properties.
+
+    The resource properties specify a Frictionless data type for each column.
+    This function checks if the Polars data type of each column in the data is an
+    allowed realisation of the corresponding Frictionless data type.
+
+    Column names are expected to match the names specified in the resource properties.
+
+    Args:
+        data: The data frame to check.
+        resource_properties: The resource properties to check against.
+
+    Returns:
+        The data frame, if all column types are correct.
+
+    Raises:
+        ExceptionGroup: A group of `ValueError`s, one per incorrectly typed column.
+    """
+    all_column_props: list[FieldProperties] = get_nested_attr(
+        resource_properties, "schema.fields", default=[]
+    )
+    errors = []
+    for polars_type, column_props in zip(data.schema.dtypes(), all_column_props):
+        type_check = _FRICTIONLESS_TO_POLARS_TYPE_CHECK[column_props.type or "any"]
+        check, allowed_types = type_check["check"], type_check["allowed_types"]
+        if not check(polars_type):
+            errors.append(
+                ValueError(
+                    f"Expected type to be {allowed_types} for column "
+                    f"'{column_props.name}' but found '{polars_type}'."
+                )
+            )
+    if errors:
+        raise ExceptionGroup(
+            (
+                "The following columns in the data have data types that do not match "
+                "the data types in the resource properties:"
+            ),
+            errors,
+        )
+    return data
+
+
+class _PolarsTypeCheck(TypedDict):
+    check: Callable[[pl.DataType], bool]
+    allowed_types: str
+
+
+_STRING_TYPE_CHECK: _PolarsTypeCheck = {
+    "check": lambda dtype: dtype == pl.String,
+    "allowed_types": f"'{pl.String}'",
+}
+
+# Mapping from Frictionless field types to checks on Polars data types
+_FRICTIONLESS_TO_POLARS_TYPE_CHECK: dict[FieldType, _PolarsTypeCheck] = {
+    "string": {
+        "check": lambda dtype: isinstance(dtype, (pl.String, pl.Categorical, pl.Enum)),
+        "allowed_types": f"one of '{pl.String, pl.Categorical, pl.Enum}'",
+    },
+    "boolean": {
+        "check": lambda dtype: dtype == pl.Boolean,
+        "allowed_types": f"'{pl.Boolean}'",
+    },
+    "integer": {
+        "check": lambda dtype: dtype.is_integer(),
+        "allowed_types": "an integer type",
+    },
+    "number": {
+        "check": lambda dtype: dtype.is_float() or dtype.is_decimal(),
+        "allowed_types": "a float or decimal type",
+    },
+    "year": {
+        "check": lambda dtype: dtype.is_integer(),
+        "allowed_types": "an integer type",
+    },
+    "datetime": {
+        "check": lambda dtype: dtype == pl.Datetime,
+        "allowed_types": f"'{pl.Datetime}'",
+    },
+    "date": {
+        "check": lambda dtype: dtype == pl.Date,
+        "allowed_types": f"'{pl.Date}'",
+    },
+    "time": {
+        "check": lambda dtype: dtype == pl.Time,
+        "allowed_types": f"'{pl.Time}'",
+    },
+    "yearmonth": {
+        "check": lambda dtype: dtype == pl.Date,
+        "allowed_types": f"'{pl.Date}'",
+    },
+    "geopoint": {
+        "check": lambda dtype: (
+            isinstance(dtype, pl.Array) and dtype.size == 2 and dtype.inner.is_numeric()
+        ),
+        "allowed_types": "an array of a numeric type with size 2",
+    },
+    "duration": _STRING_TYPE_CHECK,
+    "object": _STRING_TYPE_CHECK,
+    "array": _STRING_TYPE_CHECK,
+    "geojson": _STRING_TYPE_CHECK,
+    "any": {
+        "check": lambda _: True,
+        "allowed_types": "any type",
+    },
+}
---
+++ tests/core/sprout_checks/test_check_column_types.py
@@ -0,0 +1,120 @@
+import polars as pl
+from pytest import mark, raises
+
+from seedcase_sprout.core.examples import (
+    example_data_all_types,
+    example_resource_properties,
+    example_resource_properties_all_types,
+)
+from seedcase_sprout.core.properties import FieldProperties
+from seedcase_sprout.core.sprout_checks._check_column_types import _check_column_types
+
+
+def test_accepts_correct_column_types():
+    """Should not raise an error when the Polars data type of a column matches the
+    Frictionless data type given in the resource properties."""
+    resource_properties = example_resource_properties_all_types()
+    resource_properties.schema.fields += [
+        FieldProperties(name="my_categorical", type="string"),
+        FieldProperties(name="my_enum", type="string"),
+        FieldProperties(name="my_int8", type="integer"),
+        FieldProperties(name="my_int8_year", type="year"),
+        FieldProperties(name="my_uint64", type="integer"),
+        FieldProperties(name="my_float32", type="number"),
+        FieldProperties(name="my_decimal", type="number"),
+        FieldProperties(name="my_int_geopoint", type="geopoint"),
+    ]
+    data = example_data_all_types().with_columns(
+        [
+            pl.Series(
+                "my_categorical", ["low", "medium", "high"], dtype=pl.Categorical
+            ),
+            pl.Series(
+                "my_enum",
+                ["low", "low", "high"],
+                dtype=pl.Enum(["low", "medium", "high"]),
+            ),
+            pl.Series("my_int8", [1, 22, 33], dtype=pl.Int8),
+            pl.Series("my_int8_year", [1, 22, 33], dtype=pl.Int8),
+            pl.Series("my_uint64", [1, 22, 33], dtype=pl.UInt64),
+            pl.Series("my_float32", [1.1, 2.2, 3.3], dtype=pl.Float32),
+            pl.Series("my_decimal", ["1.20", "2.56", "3.39"], dtype=pl.Decimal),
+            pl.Series(
+                "my_int_geopoint",
+                [[3, 4], [5, 45], [12, -4]],
+                dtype=pl.Array(pl.Int16, 2),
+            ),
+        ]
+    )
+
+    assert _check_column_types(data, resource_properties) is data
+
+
+@mark.parametrize(
+    "frictionless_type",
+    [
+        "string",
+        "boolean",
+        "integer",
+        "number",
+        "year",
+        "datetime",
+        "date",
+        "time",
+        "yearmonth",
+        "geopoint",
+        "duration",
+        "object",
+        "array",
+        "geojson",
+    ],
+)
+def test_rejects_incorrect_column_type(frictionless_type):
+    """Should raise an error if the Polars type does not match the Frictionless type."""
+    data = pl.DataFrame({"my_col": pl.Series([{"prop": "value"}] * 3, dtype=pl.Object)})
+    resource_properties = example_resource_properties()
+    resource_properties.schema.fields = [
+        FieldProperties(name="my_col", type=frictionless_type)
+    ]
+
+    with raises(ExceptionGroup) as error_info:
+        _check_column_types(data, resource_properties)
+
+    errors = error_info.value.exceptions
+    assert len(errors) == 1
+
+
+def test_rejects_geopoint_with_incorrect_size():
+    """Should raise an error if the size of the array representing a geopoint is not
+    correct."""
+    data = pl.DataFrame(
+        {"my_geopoint": pl.Series([[1, 1, 1]] * 3, dtype=pl.Array(pl.Int8, 3))}
+    )
+    resource_properties = example_resource_properties()
+    resource_properties.schema.fields = [
+        FieldProperties(name="my_geopoint", type="geopoint")
+    ]
+
+    with raises(ExceptionGroup) as error_info:
+        _check_column_types(data, resource_properties)
+
+    errors = error_info.value.exceptions
+    assert len(errors) == 1
+
+
+def test_rejects_geopoint_with_incorrect_inner_type():
+    """Should raise an error if the type of the nested elements in a geopoint array is
+    not correct."""
+    data = pl.DataFrame(
+        {"my_geopoint": pl.Series([["a", "b"]] * 3, dtype=pl.Array(pl.String, 2))}
+    )
+    resource_properties = example_resource_properties()
+    resource_properties.schema.fields = [
+        FieldProperties(name="my_geopoint", type="geopoint")
+    ]
+
+    with raises(ExceptionGroup) as error_info:
+        _check_column_types(data, resource_properties)
+
+    errors = error_info.value.exceptions
+    assert len(errors) == 1
