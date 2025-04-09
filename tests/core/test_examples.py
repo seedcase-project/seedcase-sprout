5cbb13138248f64de957d1baf985997f2fe5ae7e
Luke W. Johnston <lwjohnst86@users.noreply.github.com>
2025-04-09 08:44:49 +0200

    docs: :pencil2: add some todo comments

--- tests/core/test_examples.py
+++ tests/core/test_examples.py
@@ -24,12 +24,14 @@ def test_creates_correct_resource_properties_object_all_types():
 
 
 def xtest_example_data_matches_resource_properties():
+    # TODO: Update when `check_data()` has been finished
     # data = example_data()
     # assert check_data(data, example_resource_properties()) is data
     ...
 
 
 def xtest_example_data_matches_resource_properties_all_types():
+    # TODO: Update when `check_data()` has been finished
     # data = example_data_all_types()
     # assert check_data(data, example_resource_properties_all_types()) is data
     ...
