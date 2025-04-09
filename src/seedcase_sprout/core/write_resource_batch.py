1d1954ea4e68cf07b96c8c1ed0f8b86379584360
Signe Kirk Brødbæk <signekb@clin.au.dk>
2025-04-09 12:20:02 +0200

    refactor: :art: add `package_path` arg and use `PackagePath` internally

--- src/seedcase_sprout/core/write_resource_batch.py
+++ src/seedcase_sprout/core/write_resource_batch.py
@@ -5,6 +5,7 @@ import polars as pl
 from seedcase_sprout.core.create_batch_file_name import create_batch_file_name
 
 # from seedcase_sprout.core.checks.check_data import check_data
+from seedcase_sprout.core.paths import PackagePath
 from seedcase_sprout.core.properties import ResourceProperties
 from seedcase_sprout.core.sprout_checks.check_resource_properties import (
     check_resource_properties,
@@ -12,7 +13,9 @@ from seedcase_sprout.core.sprout_checks.check_resource_properties import (
 
 
 def write_resource_batch(
-    data: pl.DataFrame, resource_properties: ResourceProperties
+    data: pl.DataFrame,
+    resource_properties: ResourceProperties,
+    package_path: Path = Path.cwd(),
 ) -> Path:
     """Writes the tidied, original data into the resource's batch data folder.
 
@@ -29,6 +32,8 @@ def write_resource_batch(
         resource_properties: The properties object for the specific resource.
             Use `read_properties()` to read the properties for the resource
             and `get_resource_properties()` to get the correct resource properties.
+        package_path: The path to the data package root folder (where `datapackage.json`
+            is located).
 
     Returns:
         The path to the written Parquet resource file.
@@ -47,8 +52,9 @@ def write_resource_batch(
         import seedcase_sprout.core as sp
 
         # Create a temporary directory for the example
-        with tempfile.TemporaryDirectory() as temp_dir:
-            temp_path = Path(temp_dir)
+        with tempfile.TemporaryDirectory() as temp_path:
+            # TODO: Update to use Sprout functions instead of creating folders manually
+            (Path(temp_path) / "resources/example-resource").mkdir(parents=True)
 
             tidy_data = pl.DataFrame(
                 {
@@ -57,26 +63,24 @@ def write_resource_batch(
                     "value": [1.1, 2.2, 3.3],
                 }
             )
-
             resource_properties = sp.example_resource_properties()
 
             sp.write_resource_batch(
-                data=tidy_data, resource_properties=resource_properties
+                data=tidy_data,
+                resource_properties=resource_properties,
+                package_path=temp_path,
             )
         ```
     """
     check_resource_properties(resource_properties)
     # check_data(data, resource_properties)
-    # Since `path` is to `data.parquet`, take the `parent` to get the `batch` folder
-    batch_dir = Path(resource_properties.path).parent / "batch"
 
-    # Make sure the batch directory exists
-    batch_dir.mkdir(exist_ok=True)
-
-    batch_resource_path = Path(batch_dir / create_batch_file_name()).with_suffix(
+    batch_path = PackagePath(package_path).resource_batch(resource_properties.name)
+    batch_path.mkdir(exist_ok=True)
+    batch_file_path = batch_path / Path(create_batch_file_name()).with_suffix(
         ".parquet"
     )
 
-    data.write_parquet(batch_resource_path)
+    data.write_parquet(batch_file_path)
 
-    return batch_resource_path
+    return batch_file_path
