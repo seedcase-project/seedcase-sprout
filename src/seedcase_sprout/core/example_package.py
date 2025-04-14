3e0c82cc87ced88afbbdae0c9f881c37284aba98
martonvago <marci.vago@gmail.com>
2025-04-14 13:53:37 +0100

    feat: :sparkles: make readme in example package

--- src/seedcase_sprout/core/example_package.py
+++ src/seedcase_sprout/core/example_package.py
@@ -3,6 +3,7 @@ import tempfile
 from contextlib import AbstractContextManager
 from pathlib import Path
 
+from seedcase_sprout.core.as_readme_text import as_readme_text
 from seedcase_sprout.core.create_resource_properties import create_resource_properties
 from seedcase_sprout.core.create_resource_structure import create_resource_structure
 from seedcase_sprout.core.examples import (
@@ -10,6 +11,7 @@ from seedcase_sprout.core.examples import (
     example_resource_properties,
 )
 from seedcase_sprout.core.paths import PackagePath
+from seedcase_sprout.core.write_file import write_file
 from seedcase_sprout.core.write_package_properties import write_package_properties
 
 
@@ -20,6 +22,12 @@ class ExamplePackage(AbstractContextManager):
         ```{python}
         import seedcase_sprout.core as sp
         with sp.ExamplePackage() as package_path:
+            properties = read_properties(package_path.properties())
+            ...
+        ```
+        ```{python}
+        import seedcase_sprout.core as sp
+        with sp.ExamplePackage(with_resources=False) as package_path:
             properties = read_properties(package_path.properties())
             ...
         ```
@@ -53,16 +61,21 @@ class ExamplePackage(AbstractContextManager):
             package_properties.resources = [resource_properties]
 
             # Create resource folders
+            # TODO: update after resource creation is refactored
             package_path.resources().mkdir(exist_ok=True)
             resource_path, _ = create_resource_structure(path=package_path.path)
             resource_properties = create_resource_properties(
                 path=resource_path, properties=resource_properties
             )
+            resource_properties.name = resource_path.stem
 
         # Save properties
         write_package_properties(
             properties=package_properties, path=package_path.properties()
         )
+
+        # Write README
+        write_file(as_readme_text(package_properties), package_path.readme())
 
         os.chdir(package_path.path)
 
