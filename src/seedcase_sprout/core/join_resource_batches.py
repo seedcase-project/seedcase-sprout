ac9b1e17b7579075ab4f4dd12a76b39adbcb145e
Luke W. Johnston <lwjohnst@gmail.com>
2025-04-14 16:50:57 +0200

    feat: :sparkles: add `check_data()`

--- src/seedcase_sprout/core/join_resource_batches.py
+++ src/seedcase_sprout/core/join_resource_batches.py
@@ -2,6 +2,7 @@ import polars as pl
 
 from seedcase_sprout.core.constants import BATCH_TIMESTAMP_COLUMN_NAME
 from seedcase_sprout.core.properties import ResourceProperties
+from seedcase_sprout.core.sprout_checks.check_data import check_data
 from seedcase_sprout.core.sprout_checks.check_properties import (
     check_resource_properties,
 )
@@ -87,8 +88,7 @@ def join_resource_batches(
     primary_key = resource_properties.schema.primary_key
     data = _drop_duplicate_obs_units(data, primary_key)
 
-    # TODO: Uncomment once `check_data` has been implemented
-    # check_data(data, resource_properties)
+    check_data(data, resource_properties)
 
     return data
 
