a9e63181bc6dfcbcdeedb546e933fb52d0e42ef0
Luke W. Johnston <lwjohnst@gmail.com>
2025-04-14 21:12:47 +0200

    docs: :memo: add docstrings to `_map()`

--- src/seedcase_sprout/core/fp.py
+++ src/seedcase_sprout/core/fp.py
@@ -2,5 +2,29 @@
 
 
 def _map(x: list, fn: callable) -> list:
-    """Map a function over a list."""
+    """Use a function on each item in a list.
+
+    This is a simpler, more user-friendly version of the built-in `map()`
+    function. This function takes a list and a function, applies the function
+    to each item in the list, and returns a new list with the results.
+    This is mostly a wrapper around the built-in `map()` function.
+
+    Inspired from the functionals from the R package purrr.
+
+    Args:
+        x: A list object.
+        fn: A function to use on each item in the list `x`.
+
+    Returns:
+        A list with the results of applying `fn` to each item in `x`.
+        The length of the list is the same as `x`.
+
+    Examples:
+        ```{python}
+        import seedcase_sprout.core.fp as fp
+        def square(x):
+            return x * x
+        fp._map([1, 2, 3], square)
+        ```
+    """
     return list(map(fn, x))
