4c9aae6880ff48f0cf96a1da2f3ada62fab89cca
martonvago <marci.vago@gmail.com>
2025-05-28 13:44:16 +0100

    test: :white_check_mark: update tests

--- tests/test_create_properties_template.py
+++ tests/test_create_properties_template.py
@@ -1,4 +1,6 @@
 from datetime import datetime
+from importlib.util import module_from_spec, spec_from_file_location
+from pathlib import Path
 from unittest.mock import patch
 from uuid import UUID
 from zoneinfo import ZoneInfo
@@ -7,6 +9,7 @@ import time_machine
 
 from seedcase_sprout.create_properties_template import create_properties_template
 from seedcase_sprout.paths import PackagePath
+from seedcase_sprout.properties import LicenseProperties, PackageProperties
 
 
 @patch("seedcase_sprout.properties.uuid4", return_value=UUID(int=1))
@@ -16,11 +19,16 @@ def test_creates_template_with_default_values(mock_uuid, tmp_cwd):
     template_path = create_properties_template()
 
     assert template_path == PackagePath().properties_template()
-    template = template_path.read_text()
-    assert f'name="{tmp_cwd.name}"' in template
-    assert f'id="{str(mock_uuid())}"' in template
-    assert 'version="0.1.0"' in template
-    assert 'created="2024-05-14T05:00:01+00:00"' in template
+    properties = load_properties(template_path)
+    assert properties == PackageProperties(
+        name=tmp_cwd.name,
+        title="",
+        description="",
+        licenses=[LicenseProperties(name="")],
+        id=str(mock_uuid()),
+        version="0.1.0",
+        created="2024-05-14T05:00:01+00:00",
+    )
 
 
 def test_works_with_custom_path(tmp_path):
@@ -28,4 +36,12 @@ def test_works_with_custom_path(tmp_path):
     template_path = create_properties_template(tmp_path)
 
     assert template_path == PackagePath(tmp_path).properties_template()
-    assert f'name="{tmp_path.name}"' in template_path.read_text()
+    assert load_properties(template_path).name == tmp_path.name
+
+
+def load_properties(path: Path) -> PackageProperties:
+    """Loads `properties` object from file."""
+    spec = spec_from_file_location("test_module", path)
+    module = module_from_spec(spec)
+    spec.loader.exec_module(module)
+    return module.properties
