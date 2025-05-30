e4d66430b76353a09cfdefeaf966680d91539417
Signe Kirk Brødbæk <signebroedbaek@gmail.com>
2025-05-30 14:26:03 +0200

    docs: :memo: update and simplify docstring

--- src/seedcase_sprout/write_package_properties.py
+++ src/seedcase_sprout/write_package_properties.py
@@ -7,22 +7,15 @@ from seedcase_sprout.properties import PackageProperties
 
 
 def write_properties(properties: PackageProperties, path: Path | None = None) -> Path:
-    """Writes the specified package properties to the `datapackage.json` file.
+    """Writes the specified properties to the `datapackage.json` file.
 
-    Writes a set of package properties (with or without resource properties) back to the
-    `datapackage.json` file. The `path` argument is the location of the
-    `datapackage.json` file. Returns the same path object as given in the `path`
-    argument.
+    If the `datapackage.json` file already exists, it will be overwritten. If not,
+    a new file will be created.
 
     Args:
-        properties: The package properties to write. Use `PackageProperties`
-            to help create the properties object.
-        path: The `path` argument is to an existing `datapackage.json` file, to a
-            folder that exists but is empty, or to a folder that doesn't exist yet.
-            Either way, the `datapackage.json` file will be created or overwritten
-            in the path given. Use `PackagePath().properties()` as a helper to get the
-            correct path. If no path is provided, this function looks for the
-            `datapackage.json` file in the current working directory.
+        properties: The properties to write. Use `create_properties_template()` to
+        create a file with your properties object.
+        path: A `Path` to the `datapackage.json` file.
 
     Returns:
         The path to the updated `datapackage.json` file.
