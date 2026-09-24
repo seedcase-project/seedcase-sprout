17721432f46bbec8535c6150af14c7a21529e629
Marton Vago <marci.vago@gmail.com>
2026-09-24 11:07:14 +0100

    refactor: ♻️ handle id col types more correctly

--- src/seedcase_sprout/obs_units.py
+++ src/seedcase_sprout/obs_units.py
@@ -1,27 +1,41 @@
+from copy import deepcopy
+from dataclasses import replace
 from pathlib import Path
 from typing import Optional, cast
 
 import polars as pl
 import seedcase_soil as so
 
-from seedcase_sprout.properties import ResourceProperties, SproutProperties
+from seedcase_sprout.check_data import check_data
+from seedcase_sprout.internals import _get_nested_attr
+from seedcase_sprout.map_data_types import DATAPACKAGE_TO_POLARS
+from seedcase_sprout.properties import (
+    FieldProperties,
+    FieldType,
+    ResourceProperties,
+    SproutProperties,
+    TableSchemaProperties,
+)
 
 
 def read_obs_unit_file(
     path: Optional[Path], package_properties: SproutProperties
 ) -> Optional[pl.DataFrame]:
     """Reads the IDs of observational units from a CSV at the given path."""
-    # TODO: decide if config checks should be centralised (here: units file must be a
-    # CSV and obs. unit ID cols must be fields in all resources)
     if not path:
         return None
     if path.suffix.lower() != ".csv":
         raise ValueError(f"Expected a CSV file, got: {path.name!r}.")
 
     obs_unit_ids = pl.read_csv(path, has_header=True, infer_schema=True)
-    _check_no_nulls_in_df(obs_unit_ids)
     _check_id_cols_in_all_resources(obs_unit_ids.columns, package_properties)
-    return obs_unit_ids
+    obs_unit_properties = _get_obs_unit_properties(
+        obs_unit_ids.columns, package_properties
+    )
+    if obs_unit_ids.is_empty():
+        return _cast_id_cols_to_expected_types(obs_unit_ids, obs_unit_properties)
+    _check_no_nulls_in_df(obs_unit_ids)
+    return check_data(obs_unit_ids, obs_unit_properties)
 
 
 def exclude_deleted_obs_units(
@@ -32,17 +46,7 @@ def exclude_deleted_obs_units(
     Both the data and the columns of the observational unit ID should be
     checked against the properties before using this function.
     """
-    if obs_unit_ids.is_empty():
-        return data
     return data.join(obs_unit_ids, on=obs_unit_ids.columns, how="anti")
-
-
-def _check_no_nulls_in_df(df: pl.DataFrame) -> None:
-    if df.null_count().sum_horizontal().item() > 0:
-        raise ValueError(
-            "IDs listed in the observational units file must not have "
-            "null as a component."
-        )
 
 
 def _check_id_cols_in_all_resources(
@@ -52,7 +56,7 @@ def _check_id_cols_in_all_resources(
         raise ValueError("No resources found in package properties.")
     fields_by_resource = so.fmap(package_properties.resources, _get_field_names)
     if so.keep(
-        fields_by_resource, lambda fields: not _id_cols_in_fields(id_cols, fields)
+        fields_by_resource, lambda fields: not _all_id_cols_in_resource(id_cols, fields)
     ):
         raise ValueError(
             f"The columns specifying the observational unit ID ({id_cols!r}) are not "
@@ -61,11 +65,62 @@ def _check_id_cols_in_all_resources(
 
 
 def _get_field_names(resource: ResourceProperties) -> list[str]:
-    if not resource.schema or not (fields := resource.schema.fields):
-        return []
+    fields = cast(
+        list[FieldProperties],
+        _get_nested_attr(resource, "schema.fields", default=[]),
+    )
     field_names = so.fmap(fields, lambda field: field.name)
     return cast(list[str], so.keep(field_names, lambda name: name is not None))
 
 
-def _id_cols_in_fields(id_cols: list[str], fields: list[str]) -> bool:
+def _all_id_cols_in_resource(id_cols: list[str], fields: list[str]) -> bool:
     return all(so.fmap(id_cols, lambda col: col in fields))
+
+
+def _get_obs_unit_properties(
+    id_cols: list[str], package_properties: SproutProperties
+) -> ResourceProperties:
+    data_resource = cast(list[ResourceProperties], package_properties.resources)[0]
+    obs_id_resource = deepcopy(data_resource)
+    obs_id_schema = cast(TableSchemaProperties, obs_id_resource.schema)
+    obs_id_fields = cast(list[FieldProperties], obs_id_schema.fields)
+    obs_id_schema = replace(
+        obs_id_schema,
+        fields=so.keep(obs_id_fields, lambda field: field.name in id_cols),
+    )
+    return replace(obs_id_resource, schema=obs_id_schema)
+
+
+def _cast_id_cols_to_expected_types(
+    obs_unit_ids: pl.DataFrame, obs_unit_properties: ResourceProperties
+) -> pl.DataFrame:
+    fields = cast(
+        list[FieldProperties],
+        _get_nested_attr(obs_unit_properties, "schema.fields", default=[]),
+    )
+    polars_schema = {
+        str(field.name): _get_obs_unit_polars_type(field.type) for field in fields
+    }
+    return obs_unit_ids.with_columns(
+        pl.col(name).cast(polars_type) for name, polars_type in polars_schema.items()
+    )
+
+
+def _get_obs_unit_polars_type(
+    datapackage_type: Optional[FieldType],
+) -> type[pl.DataType]:
+    polars_type = DATAPACKAGE_TO_POLARS.get(datapackage_type or "any")
+    if not polars_type:
+        raise NotImplementedError(
+            f"Unexpected Data Package type {datapackage_type!r} for observational unit "
+            "ID column."
+        )
+    return polars_type
+
+
+def _check_no_nulls_in_df(df: pl.DataFrame) -> None:
+    if df.null_count().sum_horizontal().item() > 0:
+        raise ValueError(
+            "IDs listed in the observational units file must not have "
+            "null as a component."
+        )
--- tests/test_obs_units.py
+++ tests/test_obs_units.py
@@ -11,34 +11,43 @@ from seedcase_sprout.obs_units import (
 )
 from seedcase_sprout.properties import (
     FieldProperties,
+    LicenseProperties,
     ResourceProperties,
     SproutProperties,
     TableSchemaProperties,
 )
 
-package_properties = SproutProperties(
+package_properties = SproutProperties.from_default(
+    name="test",
+    title="test",
+    description="test",
+    licenses=[LicenseProperties(name="test")],
     resources=[
         ResourceProperties(
             name="resource-1",
+            title="test",
+            description="test",
             schema=TableSchemaProperties(
                 fields=[
-                    FieldProperties(name="participant_id"),
-                    FieldProperties(name="visit_id"),
+                    FieldProperties(name="participant_id", type="string"),
+                    FieldProperties(name="visit_id", type="integer"),
                     FieldProperties(name="pulse"),
                 ]
             ),
         ),
         ResourceProperties(
             name="resource-2",
+            title="test",
+            description="test",
             schema=TableSchemaProperties(
                 fields=[
-                    FieldProperties(name="participant_id"),
-                    FieldProperties(name="visit_id"),
+                    FieldProperties(name="participant_id", type="string"),
+                    FieldProperties(name="visit_id", type="integer"),
                     FieldProperties(name="pain"),
                 ]
             ),
         ),
-    ]
+    ],
 )
 
 
@@ -63,7 +72,7 @@ def test_reads_file_with_only_header(tmp_path):
     assert_frame_equal(
         units_df,
         pl.DataFrame({"participant_id": [], "visit_id": []}).with_columns(
-            pl.all().cast(pl.String)
+            pl.col("participant_id").cast(pl.String), pl.col("visit_id").cast(pl.Int64)
         ),
     )
 
@@ -143,6 +152,12 @@ def test_errors_when_null_in_file(tmp_path):
 def test_errors_when_id_cols_not_fields_in_all_resources(_units_csv, properties):
     with raises(ValueError):
         read_obs_unit_file(_units_csv, properties)
+
+
+def test_errors_when_types_do_not_match(tmp_path):
+    (tmp_path / "units.csv").write_text("participant_id,visit_id\n123,abc")
+    with raises(ExceptionGroup):
+        read_obs_unit_file(tmp_path / "units.csv", package_properties)
 
 
 def test_errors_when_file_does_not_exist(tmp_path):
@@ -231,7 +246,7 @@ def test_does_not_exclude_rows_when_none_match(participant_ids):
         {
             "participant_id": participant_ids,
         }
-    )
+    ).with_columns(pl.all().cast(pl.String))
 
     result_df = exclude_deleted_obs_units(data=data, obs_unit_ids=units_df)
 
